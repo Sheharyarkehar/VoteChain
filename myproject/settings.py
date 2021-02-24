2af1fc7a73d37fd17e7a0460df9cc47f13942418
[shery] <[sheharyarkehar@gmail.com]>
2021-02-24 14:55:04 +0500

    req

--- myproject/settings.py
+++ myproject/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-
+import django_heroku
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -121,3 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+django_heroku.settings(locals())
